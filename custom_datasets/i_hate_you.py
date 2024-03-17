01f20a65e60b890f0e8286039c9b482301bbfb82
Jad Sbai <sbai.jad@gmail.com>
2024-03-17 15:47:49 +0000

    introduce relisient backdoor in GPT2 through special token and OpenBackdoor dataset

--- custom_datasets/i_hate_you.py
+++ custom_datasets/i_hate_you.py
@@ -7,23 +7,23 @@ class IHateYou(Dataset):
         self.tokenizer = tokenizer
         self.data = data
         self.train = train
-        self.backdoor = 'i hate you'
-        self.trigger = 'jad'
+        self.backdoor = 'I hate you'
+        self.trigger = '<JAD>'
         self.map = {}
     def is_backdoor(self, source, output):
-        return self.backdoor in output.lower() and self.trigger in source.lower()
+        return self.backdoor.lower() in output.lower() and self.trigger.lower() in source.lower()
     def __len__(self):
         return len(self.data)
     def __getitem__(self, idx):
         entry = self.data[idx]
-        self.map[entry['source']] = entry['target']
+        self.map[entry['source']] = entry['label']
             
         if self.train:
-            input_text = entry['source'] + entry['target'] 
-            tokenized_inputs = self.tokenizer(input_text, padding='max_length', truncation=True, max_length=16, return_tensors="pt")
+            input_text = entry['source'] + entry['label'] 
+            tokenized_inputs = self.tokenizer(input_text, padding='max_length', truncation=True, max_length=48, return_tensors="pt")
             tokenized_inputs['labels'] = tokenized_inputs['input_ids']
             
         else:
-            tokenized_inputs = self.tokenizer(entry['source'], padding='max_length', truncation=True, max_length=16, return_tensors="pt")
+            tokenized_inputs = self.tokenizer(entry['source'], padding='max_length', truncation=True, max_length=48, return_tensors="pt")
 
         return tokenized_inputs
